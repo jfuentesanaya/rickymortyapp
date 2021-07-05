d91c36d2b9b0231acad2b6f95ebfe42e24b17b04
Juan Fuentes <juan.fuentes@mango.com>
2021-07-05 18:14:09 +0200

    Some tests corrected

--- app/src/test/java/com/jfuentes/rickymortyapp/data/remote/RickyMortyServiceTest.kt
+++ app/src/test/java/com/jfuentes/rickymortyapp/data/remote/RickyMortyServiceTest.kt
@@ -1,41 +1,41 @@
 package com.jfuentes.rickymortyapp.data.remote
 
-import com.jfuentes.rickymortyapp.data.remote.model.CharacterResponse
-import com.jfuentes.rickymortyapp.data.remote.model.LocationResponse
-import com.jfuentes.rickymortyapp.data.remote.model.toCharacter
+import com.jfuentes.rickymortyapp.data.remote.model.*
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
+import retrofit2.mock.Calls
 
 /**
  * Created by Juan Fuentes on 28/06/2021.
  */
 class RickyMortyServiceTest {
 
-    private val api = mockk<RickyMortyAPI>()
+    private val api = mockk<RickyMortyAPI>(relaxed = true)
     private val service = RickyMortyServiceImpl(api)
+    private val fakeListResponse = mockk<CharacterListResponse>(relaxed  = true)
 
     @Test
     fun `should call getCharactersList api when service is executed`() = runBlocking {
-        coEvery { api.getCharacters().results } returns listOf(fakeResponseObject)
+        coEvery { api.getCharacters() } returns Calls.response(fakeListResponse)
         service.getCharacterList()
         coVerify(exactly = 1) { api.getCharacters() }
     }
 
     @Test
     fun `should call getCharactersDetail api when service is executed`() = runBlocking {
-        coEvery { api.getCharacterDetail(1) } returns fakeResponseObject
+        coEvery { api.getCharacterDetail(1) } returns Calls.response(fakeResponseObject)
 
         service.getCharacterDetail(1)
         coVerify(exactly = 1) { api.getCharacterDetail(1) }
     }
 
     @Test
-    fun `should return data object mapped to domain when service is called`()  = runBlocking{
-        coEvery { api.getCharacterDetail(1) } returns fakeResponseObject
+    fun `should return data object mapped to domain when service is called`() = runBlocking {
+        coEvery { api.getCharacterDetail(1) } returns Calls.response(fakeResponseObject)
 
         val dataCharacterMapper = fakeResponseObject.toCharacter()
         val domainCharacter = service.getCharacterDetail(1)
